d681128d07bd08bcdeb8b436605ba544cd25494d
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-09-16 19:18:33 +0700

    login check token

--- src/pages/Account/login.js
+++ src/pages/Account/login.js
@@ -5,16 +5,17 @@ import 'react-toastify/dist/ReactToastify.css';
 import { httpRequest } from '~/utils/httpRequest';
 import { jwtDecode } from 'jwt-decode';
 import { isAuthenticated } from '~/utils/httpRequest';
+
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
-      if (isAuthenticated()) {
-          navigate('/');
-      }
-  }, []);
+        if (isAuthenticated()) {
+            navigate('/');
+        }
+    }, [navigate]);
 
     const handleLogin = async (event) => {
         event.preventDefault();
@@ -22,7 +23,7 @@ function Login() {
         if (validate()) {
             try {
                 const response = await httpRequest.post(
-                    'http://localhost:8081/api/v1/auth/login',
+                    'http://localhost:8080/api/v1/auth/login',
                     {
                         username: username,
                         password: password,
@@ -35,26 +36,26 @@ function Login() {
 
                     try {
                         decodedToken = jwtDecode(accessToken);
-                        console.log('Decoded Token:', decodedToken);
 
                         const id = response.data.id;
                         const roles = response.data.roles;
+                        const refreshToken = response.data.refreshToken;
 
                         if (!roles.includes('ROLE_ADMIN')) {
-                            toast.warn('username and password wrong');
+                            toast.warn('Username and password are incorrect');
                             return;
                         }
 
                         localStorage.setItem('roles', roles);
                         localStorage.setItem('accessToken', accessToken);
+                        localStorage.setItem('refreshToken', refreshToken);
                         localStorage.setItem('id', id);
                         localStorage.setItem('username', username);
 
                         navigate('/');
                         window.location.reload();
                     } catch (error) {
-                        toast.error('Failed to decode token.');
-                        return;
+                        return toast.error('Failed to decode token.');
                     }
                 } else {
                     toast.error('Invalid response from server');
@@ -76,83 +77,82 @@ function Login() {
         }
         return true;
     };
-    
 
     return (
-      <>
-        <div className="container-scroller">
-            <div className="container-fluid page-body-wrapper full-page-wrapper">
-                <div className="content-wrapper d-flex align-items-center auth px-0">
-                    <div className="row w-100 mx-0">
-                        <div className="col-lg-4 mx-auto">
-                            <div className="auth-form-light text-left py-5 px-4 px-sm-5">
-                                <div className="brand-logo">
-                                    <img src="src/assets/images/logo.svg" alt="logo" />
-                                </div>
-                                <h4>Hello! let's get started</h4>
-                                <h6 className="font-weight-light">Sign in to continue.</h6>
-                                <form className="pt-3" onSubmit={handleLogin}>
-                                    <div className="form-group">
-                                        <input
-                                            type="text"
-                                            value={username}
-                                            onChange={(e) => setUsername(e.target.value)}
-                                            placeholder="Username"
-                                            className="form-control"
-                                        />
+        <>
+            <div className="container-scroller">
+                <div className="container-fluid page-body-wrapper full-page-wrapper">
+                    <div className="content-wrapper d-flex align-items-center auth px-0">
+                        <div className="row w-100 mx-0">
+                            <div className="col-lg-4 mx-auto">
+                                <div className="auth-form-light text-left py-5 px-4 px-sm-5">
+                                    <div className="brand-logo">
+                                        <img src="src/assets/images/logo.svg" alt="logo" />
                                     </div>
-                                    <div className="form-group">
-                                        <input
-                                            type="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            placeholder="Password"
-                                            className="form-control"
-                                        />
-                                    </div>
-                                    <div className="mt-3">
-                                        <button
-                                            type="submit"
-                                            className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
-                                        >
-                                            SIGN IN
-                                        </button>
-                                    </div>
-                                    <div className="my-2 d-flex justify-content-between align-items-center">
-                                        <div className="form-check">
-                                            <label className="form-check-label text-muted">
-                                                <input type="checkbox" className="form-check-input" />
-                                                Keep me signed in
-                                            </label>
+                                    <h4>Hello! let's get started</h4>
+                                    <h6 className="font-weight-light">Sign in to continue.</h6>
+                                    <form className="pt-3" onSubmit={handleLogin}>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                value={username}
+                                                onChange={(e) => setUsername(e.target.value)}
+                                                placeholder="Username"
+                                                className="form-control"
+                                            />
                                         </div>
-                                        <Link to="#" className="auth-link text-black">
-                                            Forgot password?
-                                        </Link>
-                                    </div>
-                                    <div className="mb-2">
-                                        <button
-                                            type="button"
-                                            className="btn btn-block btn-facebook auth-form-btn"
-                                        >
-                                            <i className="ti-facebook mr-2" />
-                                            Connect using facebook
-                                        </button>
-                                    </div>
-                                    <div className="text-center mt-4 font-weight-light">
-                                        Don't have an account?
-                                        <Link to="/register" className="text-primary">
-                                            Create
-                                        </Link>
-                                    </div>
-                                </form>
+                                        <div className="form-group">
+                                            <input
+                                                type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                placeholder="Password"
+                                                className="form-control"
+                                            />
+                                        </div>
+                                        <div className="mt-3">
+                                            <button
+                                                type="submit"
+                                                className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                                            >
+                                                SIGN IN
+                                            </button>
+                                        </div>
+                                        <div className="my-2 d-flex justify-content-between align-items-center">
+                                            <div className="form-check">
+                                                <label className="form-check-label text-muted">
+                                                    <input type="checkbox" className="form-check-input" />
+                                                    Keep me signed in
+                                                </label>
+                                            </div>
+                                            <Link to="#" className="auth-link text-black">
+                                                Forgot password?
+                                            </Link>
+                                        </div>
+                                        <div className="mb-2">
+                                            <button
+                                                type="button"
+                                                className="btn btn-block btn-facebook auth-form-btn"
+                                            >
+                                                <i className="ti-facebook mr-2" />
+                                                Connect using facebook
+                                            </button>
+                                        </div>
+                                        <div className="text-center mt-4 font-weight-light">
+                                            Don't have an account?
+                                            <Link to="/register" className="text-primary">
+                                                Create
+                                            </Link>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <ToastContainer />
-      </>
+            <ToastContainer />
+        </>
     );
 }
 
