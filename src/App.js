865532ba2a41a364646dfd8a85a6f0418bfb9fd5
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-11-21 15:04:01 +0700

    config toggle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import { ToastContainer } from 'react-toastify';
 function App() {
     return (
         <Router>
@@ -30,18 +29,6 @@ function App() {
                         );
                     })}
                 </Routes>
-                <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
             </div>
         </Router>
     );
